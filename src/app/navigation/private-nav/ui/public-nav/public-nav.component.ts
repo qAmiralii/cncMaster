7eb9740ed22cbbc763199c349a71c1537c22d08f
qamiralii <a.bitpins@gmail.com>
2025-05-26 00:05:16 +0330

    finish half of head

--- src/app/navigation/private-nav/ui/public-nav/public-nav.component.ts
+++ src/app/navigation/private-nav/ui/public-nav/public-nav.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
-
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-public-nav',
   imports: [
     MatCardModule,
+    MatButtonModule,
 
   ],
   templateUrl: './public-nav.component.html',
