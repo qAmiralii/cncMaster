f0c67d0913e5d8b52b198a08c534842855c14319
qamiralii <a.bitpins@gmail.com>
2025-05-29 16:55:03 +0330

    finish slider

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-slider',
@@ -8,4 +8,38 @@ import { Component } from '@angular/core';
 })
 export class SliderComponent {
 
+  images: string[] = [
+    '/img/s1.png',
+    '/img/s2.png',
+    '/img/s3.png',
+    '/img/s4.png',
+    '/img/s5.png',
+
+  ];
+  currentSlide = 0;
+  intervalId: any;
+
+  ngOnInit(): void {
+    this.startAutoSlide();
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.intervalId);
+  }
+
+  startAutoSlide(): void {
+    this.intervalId = setInterval(() => {
+      this.nextSlide();
+    }, 5000);
+  }
+
+  nextSlide(): void {
+    this.currentSlide = (this.currentSlide + 1) % this.images.length;
+  }
+
+  prevSlide(): void {
+    this.currentSlide =
+      (this.currentSlide - 1 + this.images.length) % this.images.length;
+  }
 }
+
--- src/app/pages/public/home/ui/home.component.ts
+++ src/app/pages/public/home/ui/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { SliderComponent } from "../../../../components/slider/slider.component";
 @Component({
   selector: 'app-home',
   imports: [
     MatCardModule,
-  ],
+    SliderComponent
+],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
