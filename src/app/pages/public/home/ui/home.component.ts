9d45c7be4382600ae9ce530fcbbfb3050b5b0a87
qamiralii <a.bitpins@gmail.com>
2025-05-29 18:05:12 +0330

    finish icon-badge-snack bar

--- src/app/pages/public/home/ui/home.component.ts
+++ src/app/pages/public/home/ui/home.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { SliderComponent } from "../../../../components/slider/slider.component";
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatBadgeModule } from '@angular/material/badge';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-home',
@@ -12,14 +13,40 @@ import { MatBadgeModule } from '@angular/material/badge';
     MatButtonModule,
     MatBadgeModule,
     MatCardModule,
-    SliderComponent
+    SliderComponent,
+    MatSnackBarModule
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
   counter = 24;
+  disable = false;
   plus() {
     this.counter = (this.counter + 1)
+    this.disable = true
   }
+
+
+
+  durationInSeconds = 5;
+  private _snackBar = inject(MatSnackBar);
+
+  openSnackBar() {
+    this._snackBar.openFromComponent(PizzaPartyComponent, {
+      duration: this.durationInSeconds * 1000,
+    });
+  }
+
+
 }
+@Component({
+  selector: 'snack-bar-component-example-snack',
+  template: `<span class="example-pizza-party"> Many thanks! 😍</span>`,
+  styles: `
+    .example-pizza-party {
+      color: green;
+    }
+  `,
+})
+export class PizzaPartyComponent { }
