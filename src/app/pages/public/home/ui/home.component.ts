7d563f438af2bdfe21a4a241d83333e43b2850ff
qamiralii <a.bitpins@gmail.com>
2025-05-30 14:04:26 +0330

    start middle cards

--- src/app/pages/public/home/ui/home.component.ts
+++ src/app/pages/public/home/ui/home.component.ts
@@ -1,10 +1,12 @@
-import { Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { SliderComponent } from "../../../../components/slider/slider.component";
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+
+
 
 @Component({
   selector: 'app-home',
@@ -14,8 +16,9 @@ import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
     MatBadgeModule,
     MatCardModule,
     SliderComponent,
-    MatSnackBarModule
+    MatSnackBarModule,
   ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
@@ -34,7 +37,7 @@ export class HomeComponent {
 
   openSnackBar() {
     this._snackBar.openFromComponent(PizzaPartyComponent, {
-      duration: this.durationInSeconds * 1000,
+      duration: this.durationInSeconds * 9000,
     });
   }
 
@@ -43,10 +46,7 @@ export class HomeComponent {
 @Component({
   selector: 'snack-bar-component-example-snack',
   template: `<span class="example-pizza-party"> Many thanks! 😍</span>`,
-  styles: `
-    .example-pizza-party {
-      color: green;
-    }
-  `,
+  styleUrl: './home.component.scss'
+
 })
 export class PizzaPartyComponent { }
