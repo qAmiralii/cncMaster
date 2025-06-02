42df1b94ac8806e222a21b35cca344a510cc819f
qamiralii <a.bitpins@gmail.com>
2025-06-02 13:42:50 +0330

    finish model & moc data

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/public/login/ui/login.component';
 import { PublicNavComponent } from './navigation/public-nav/ui/public-nav.component';
 import { HomeComponent } from './pages/public/home/ui/home.component';
+import { ProductsComponent } from './pages/public/products/ui/products.component';
 
 export const routes: Routes = [
     {
         path: 'public', component: PublicNavComponent, children: [
             {path: 'home', component: HomeComponent, pathMatch: 'prefix'},
-            {path: '', redirectTo: 'home', pathMatch: 'prefix'}
+            {path: '', redirectTo: 'home', pathMatch: 'prefix'},
+            {path: 'products', component: ProductsComponent, pathMatch:'full'}
 
         ]
     },
--- src/app/pages/public/products/service/products.service.ts
+++ src/app/pages/public/products/service/products.service.ts
@@ -1,9 +1,43 @@
 import { Injectable } from '@angular/core';
-
+import { Observable, of } from 'rxjs';
+import { Product } from '../model/product.model';
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-  constructor() { }
+  private products: Product[] = [
+        {
+      id: 1,
+      title: 'CNC Machine X200',
+      price: 3200,
+      image: '/img/cncrouter/101.png',
+      description: 'High precision CNC machine.',
+      rating: 4.5,
+      tags: ['industrial']
+    },
+    {
+      id: 2,
+      title: 'CNC Router Pro',
+      price: 1800,
+      image: '/img/cncrouter/102.png',
+      description: 'Compact router for small-scale work.',
+      rating: 4.2,
+      tags: ['compact', 'affordable']
+    },
+    {
+      id: 3,
+      title: 'Laser CNC Cutter',
+      price: 2500,
+      image: '/img/wallPanel/wall101.png',
+      description: 'Laser precision for detailed cutting.',
+      rating: 4.7,
+      tags: ['laser', 'precision']
+    }
+
+
+
+
+
+  ]
 }
