248216ddc0f7b426f5488e939863b637991caf5a
qamiralii <a.bitpins@gmail.com>
2025-06-02 14:49:48 +0330

    before big change

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,15 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/public/login/ui/login.component';
 import { PublicNavComponent } from './navigation/public-nav/ui/public-nav.component';
 import { HomeComponent } from './pages/public/home/ui/home.component';
-import { ProductsComponent } from './pages/public/products/product/ui/products.component';
+import { ProductListComponent } from './pages/public/product-card/product-list/ui/product-list.component';
+import { ProductCardComponent } from './pages/public/product-card/ui/product-card.component';
 
 export const routes: Routes = [
     {
         path: 'public', component: PublicNavComponent, children: [
             {path: 'home', component: HomeComponent, pathMatch: 'prefix'},
             {path: '', redirectTo: 'home', pathMatch: 'prefix'},
-            {path: 'products', component: ProductsComponent, pathMatch:'full'}
+            {path: 'products', component: ProductListComponent, pathMatch:'full'}
 
         ]
     },
