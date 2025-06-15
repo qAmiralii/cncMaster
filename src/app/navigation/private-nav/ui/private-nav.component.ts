2f5be2dad119dde08d67fcfc2a38c73b19a00b44
qamiralii <a.bitpins@gmail.com>
2025-06-15 22:48:10 +0330

    admin dashboard_4

--- src/app/navigation/private-nav/ui/private-nav.component.ts
+++ src/app/navigation/private-nav/ui/private-nav.component.ts
@@ -7,15 +7,10 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import {MatListModule} from '@angular/material/list';
+import { MatListModule } from '@angular/material/list';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { Title } from '@angular/platform-browser';
 
-export interface Tile {
-  color: string;
-  cols: number;
-  rows: number;
-  text: string;
-}
 @Component({
   selector: 'app-private-nav',
   templateUrl: './private-nav.component.html',
@@ -33,38 +28,26 @@ export interface Tile {
   ]
 })
 export class PrivateNavComponent {
-  private breakpointObserver = inject(BreakpointObserver);
-
-  /** Based on the screen size, switch from standard to one column per row */
-  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+  
+  private mobileMode = inject(BreakpointObserver);
+  cards = this.mobileMode.observe(Breakpoints.Handset).pipe(
     map(({ matches }) => {
       if (matches) {
         return [
-          { title: 'Card 1', cols: 1, rows: 1 },
-          { title: 'Card 2', cols: 1, rows: 1 },
-          { title: 'Card 3', cols: 1, rows: 1 },
-          { title: 'Card 4', cols: 1, rows: 1 }
-        ];
+          { title: 'Admin Dasshboard', cols: 1, rows: 1 },
+          { title: 'Side menu', cols: 1, rows: 1 },
+          { title: '', cols: 1, rows: 1 },
+        ]
       }
-
+      
       return [
-        { title: 'Card 1', cols: 2, rows: 1 },
-        { title: 'Card 2', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 2 },
-        { title: 'Card 4', cols: 1, rows: 1 }
-      ];
+        { title: 'Admin Dasshboard', cols: 4, rows: 2 },
+        { title: 'side menu', cols: 3, rows: 8 },
+        { title: '', cols: 1, rows: 8 },
+      ]
     })
-  );
-
-
-
-
-
-
-
-
-
-
+  )
+  
+  
+  
 }
-
-
